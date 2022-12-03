2ea1e040dbd7c50c011d264cc66ee27f3e870f1f
Dave Rayment <david.rayment@experian.com>
2022-12-03 14:08:37 +0000

    Add project files.

---
+++ Day 2/Program.cs
@@ -0,0 +1,24 @@
+﻿var lines = File.ReadAllLines("Input.txt");
+Console.WriteLine(lines.Sum(line => Score(line, false)));
+Console.WriteLine(lines.Sum(line => Score(line, true)));
+
+static int Score(string line, bool isDayTwo)
+{
+	// Loss/Draw/Win responses for an opponent playing Rock, Paper, Scissors.
+	string[] allPlays = { "ZXY", "XYZ", "YZX" };
+
+	// Loss/Draw/Win responses to the current opponent.
+	string replies = allPlays[line[0] - 'A'];
+
+	if (isDayTwo)
+	{
+		// Rewrite line to be a win, loss or draw.
+		line = line[0] + " " + replies[line[2] - 'X'];
+	}
+
+	// Multiply the index of the match by 3 to get the score.
+	int score = replies.IndexOf(line[2]) * 3;
+
+	// Add the value of Rock, Paper or Scissors and return.
+	return 1 + (line[2] - 'X') + score;
+}
---
+++ Day1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine(ElfCalories().Max());
+Console.WriteLine(ElfCalories().OrderByDescending(x => x).Take(3).Sum());
+
+static IEnumerable<int> ElfCalories()
+{
+	int calories = 0;
+	foreach (string line in File.ReadAllLines("Input.txt"))
+	{
+		if (line.Length == 0)
+		{
+			yield return calories;
+			calories = 0;
+		}
+		else
+		{
+			calories += int.Parse(line);
+		}
+	}
+}
