fff8a0bae943d4db8a2efd21e3389a436f70f04a
Dave Rayment <david.rayment@experian.com>
2022-12-03 15:52:33 +0000

    Reduce Day 3 to single LINQ queries

--- Day 3/Program.cs
+++ Day 3/Program.cs
@@ -1,14 +1,13 @@
 ﻿string[] lines = File.ReadAllLines("Input.txt");
-int total = 0;
-foreach (string line in lines)
-{
-	var parts = line.Chunk(line.Length / 2).ToArray();
-	total += Priority(parts[0].Distinct().First(ch => parts[1].Contains(ch)));
-}
-Console.WriteLine(total);
 
-total = lines.Chunk(3).Sum(chunk =>
-	Priority(chunk[0].Distinct().Single(ch => chunk[1].Contains(ch) && chunk[2].Contains(ch))));
-Console.WriteLine(total);
+Console.WriteLine(lines.Sum(line =>
+{
+	var halves = line.Chunk(line.Length / 2).ToArray();
+	return Priority(halves[0].Distinct().First(ch => halves[1].Contains(ch)));
+}));
+
+Console.WriteLine(lines.Chunk(3).Sum(chunk =>
+	Priority(chunk[0].Distinct().Single(ch => 
+		chunk[1].Contains(ch) && chunk[2].Contains(ch)))));
 
 static int Priority(char ch) => ch > 'Z' ? ch - 'a' + 1 : ch - 'A' + 27;
