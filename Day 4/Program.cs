60a5fc1e83070c4dc0107709a9eb6484953b1643
Dave Rayment <dave.rayment@gmail.com>
2022-12-04 15:06:52 +0000

    Day 4

---
+++ Day 4/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine(NextChunk().Count(c => (c[0] >= c[2] && c[1] <= c[3]) || (c[2] >= c[0] && c[3] <= c[1])));
+Console.WriteLine(NextChunk().Count(c => (c[0] >= c[2] && c[0] <= c[3]) || (c[0] <= c[2] && c[2] <= c[1])));
+
+static IEnumerable<int[]> NextChunk() => File.ReadLines("Input.txt")
+	.SelectMany(line => line.Split(',')
+	.SelectMany(half => half.Split('-'))
+	.Select(x => int.Parse(x))
+	.Chunk(4));
