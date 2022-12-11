0f63e932992806942b5e9c195702307706a6b8d8
Dave Rayment <david.rayment@experian.com>
2022-12-11 17:42:57 +0000

    Day 10

---
+++ Day 10/Program.cs
@@ -0,0 +1,34 @@
+﻿int regX = 1;
+int cycle = 0;
+int signalStrength = 0;
+bool isAdd;
+
+foreach (string line in File.ReadLines("Input.txt"))
+{
+	string[] parts = line.Split(' ');
+	isAdd = parts.Length > 1;
+	int nextEvent = cycle + (isAdd ? 2 : 1);
+
+	while (cycle != nextEvent)
+	{
+		int pixelPos = cycle % 40;
+		Console.Write((pixelPos >= regX - 1 && pixelPos <= regX + 1) ? "#" : ".");
+		if (pixelPos == 0)
+		{
+			Console.WriteLine("");
+		}
+
+		cycle++;
+		if ((cycle + 20) % 40 == 0)
+		{
+			signalStrength += cycle * regX;
+		}
+	}
+
+	if (isAdd)
+	{
+		regX += int.Parse(parts[1]);
+	}
+}
+
+Console.WriteLine(signalStrength);
