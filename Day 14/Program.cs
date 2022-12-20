a4d1420a26a1a095b229d89f11132f46a41640e9
Dave Rayment <david.rayment@experian.com>
2022-12-20 22:45:47 +0000

    Day 14. Needs refactoring.

--- Day 14/Program.cs
+++ Day 14/Program.cs
@@ -1,37 +1,107 @@
-﻿var allCoords = File.ReadLines("Example.txt")
+﻿(int Dx, int Dy)[] validMoves = { (0, 1), (-1, 1), (1, 1), (0, 0) };
+
+var allCoords = File.ReadLines("Input.txt")
 	.Select(line => line.Split(" -> ")
 		.Select(x => x.Split(',')).ToArray()
 		.Select(coord => new Coord(int.Parse(coord[0]), int.Parse(coord[1]))).ToList());
 
 var coordEnum = allCoords.SelectMany(list => list.Select(coord => coord));
 Coord min = new(coordEnum.Min(xy => xy.X), 0);
-Coord max = new(coordEnum.Max(xy => xy.X), coordEnum.Max(xy => xy.Y));
-var grid = new char[max.X - min.X + 1, max.Y + 1];
+Coord max = new(coordEnum.Max(xy => xy.X), coordEnum.Max(xy => xy.Y + 2));
 
-foreach (var list in allCoords)
+// Width needs to accommodate a 'full' cavern for Day 2.
+int width = max.Y * 2 + 1;
+char[,] grid;
+
+Console.WriteLine(Simulate());
+Console.WriteLine(Simulate(true) + 1);
+Console.WriteLine("");
+
+int Simulate(bool isDayTwo = false)
 {
-	Coord last = list[0];
-	for (int i = 1; i < list.Count; i++)
+	InitGrid();
+	int sandCount = 0;
+	while (IsSandInBounds(isDayTwo))
 	{
-		DrawLine(last, list[i]);
-		last = list[i];
+		sandCount++;
+	}
+	DrawGrid();
+	return sandCount;
+}
+
+void InitGrid()
+{
+	grid = new char[width, max.Y];
+	foreach (var list in allCoords)
+	{
+		Coord last = list[0];
+		for (int i = 1; i < list.Count; i++)
+		{
+			DrawLine(last, list[i]);
+			last = list[i];
+		}
 	}
 }
 
-DrawGrid();
-Console.WriteLine("");
+bool IsSandInBounds(bool isDayTwo)
+{
+	(int X, int Y) sand = (width / 2, 0);
+	var start = sand;
+	(int X, int Y) move;
+	do
+	{
+		move = GetMove(sand, isDayTwo);
+		sand.X += move.X;
+		sand.Y += move.Y;
+	} while (move != (0, 0) && move != (-1, -1));
+
+	if (move == (-1, -1))
+	{
+		return false;
+	}
+	else
+	{
+		grid[sand.X, sand.Y] = 'o';
+		return !(sand == start);
+	}
+}
+
+bool InBounds((int x, int y) xy, bool isDayTwo)
+{
+	return isDayTwo ? true : xy.y < grid.GetLength(1);
+	//return xy.x >= 0 && xy.x < grid.GetLength(0) && xy.y < grid.GetLength(1);
+}
+
+(int X, int Y) GetMove((int X, int Y) sand, bool isDayTwo)
+{
+	foreach ((int dx, int dy) move in validMoves)
+	{
+		int newX = sand.X + move.dx;
+		int newY = sand.Y + move.dy;
+		if (!InBounds((newX, newY), isDayTwo))
+		{
+			return (-1, -1);
+		}
+		// Take into account the infinite floor for day 2.
+		char gridChar = (isDayTwo && newY == max.Y) ? '#' : grid[newX, newY];
+		if (gridChar == (char)0)
+		{
+			return move;
+		}
+	}
+
+	return (0, 0);
+}
 
 void DrawLine(Coord last, Coord next)
 {
-	int dx = next.X - last.X;
-	int dy = next.Y - last.Y;
-	int numMoves = Math.Abs(dx) + Math.Abs(dy);
-	(int currentX, int currentY) = (last.X - min.X, last.Y);
-	for (int i = 0; i < numMoves; i++)
+	grid[width / 2 + last.X - 500, last.Y] = '#';
+	while (last != next)
 	{
-		grid[currentX, currentY] = '#';
-		currentX += dx == 0 ? 0 : (dx < 0 ? -1 : 1);
-		currentY += dy == 0 ? 0 : (dy < 0 ? -1 : 1);
+		var coord = new Coord(last.X + (next.X == last.X ? 0 : (next.X > last.X ? 1 : -1)),
+			last.Y + (next.Y == last.Y ? 0 : (next.Y > last.Y ? 1 : -1)));
+		grid[width / 2 + coord.X - 500, coord.Y] = '#';
+		last = coord;
 	}
 }
 
@@ -44,11 +114,12 @@ void DrawGrid()
 			Console.Write(grid[x, y] switch
 			{
 				(char)0 => '.',
-				_ => '#'
+				_ => grid[x, y]
 			});
 		}
 		Console.WriteLine("");
 	}
+	Console.WriteLine("");
 }
 
 record Coord(int X, int Y);
