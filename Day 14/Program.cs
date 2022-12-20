792c13ea8e2f2c3e449e8e6e9f2b288c8001ac03
Dave Rayment <dave.rayment@gmail.com>
2022-12-20 00:59:47 +0000

    Day 14 start.

---
+++ Day 14/Program.cs
@@ -0,0 +1,58 @@
+﻿List<List<Coord>> allCoords = new();
+
+foreach (string line in File.ReadLines("Example.txt"))
+{
+	allCoords.Add(line.Split(" -> ").Select(x => x.Split(',')).ToArray()
+		.Select(coord => new Coord(int.Parse(coord[0]), int.Parse(coord[1])))
+		.ToList());
+}
+
+var coordEnum = allCoords.SelectMany(list => list.Select(coord => coord));
+Coord min = new(coordEnum.Min(xy => xy.X), 0);
+Coord max = new(coordEnum.Max(xy => xy.X), coordEnum.Max(xy => xy.Y));
+var grid = new char[max.X - min.X + 1, max.Y + 1];
+
+foreach (var list in allCoords)
+{
+	Coord last = list[0];
+	for (int i = 1; i < list.Count; i++)
+	{
+		DrawLine(grid, last, list[i]);
+		last = list[i];
+	}
+}
+
+DrawGrid();
+Console.WriteLine("");
+
+void DrawLine(char[,] grid, Coord last, Coord next)
+{
+	int dx = next.X - last.X;
+	int dy = next.Y - last.Y;
+	int numMoves = Math.Abs(dx) + Math.Abs(dy);
+	(int currentX, int currentY) = (last.X - min.X, last.Y);
+	for (int i = 0; i < numMoves; i++)
+	{
+		grid[currentX, currentY] = '#';
+		currentX += dx == 0 ? 0 : (dx < 0 ? -1 : 1);
+		currentY += dy == 0 ? 0 : (dy < 0 ? -1 : 1);
+	}
+}
+
+void DrawGrid()
+{
+	for (int y = 0; y < grid.GetLength(1); y++)
+	{
+		for (int x = 0; x < grid.GetLength(0); x++)
+		{
+			Console.Write(grid[x, y] switch
+			{
+				(char)0 => '.',
+				_ => '#'
+			});
+		}
+		Console.WriteLine("");
+	}
+}
+
+record Coord(int X, int Y);
