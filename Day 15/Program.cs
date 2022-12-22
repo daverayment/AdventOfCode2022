d2043ed9ad1c37f918c4f5d3b966bd31114e1f7c
Dave Rayment <david.rayment@experian.com>
2022-12-22 16:54:05 +0000

    Day 15 part 2

--- Day 15/Program.cs
+++ Day 15/Program.cs
@@ -1,40 +1,76 @@
 ﻿using System.Text.RegularExpressions;
 
+const int Day1Y = 2000000;
+
 List<Pair> pairs = File.ReadLines("Input.txt").Select(line => new Pair(line)).ToList();
+int xMin = pairs.Min(p => Math.Min(p.Beacon.X, p.Sensor.X));
+int xMax = pairs.Max(p => Math.Max(p.Beacon.X, p.Sensor.X));
+int yMin = pairs.Min(p => Math.Min(p.Beacon.Y, p.Sensor.Y));
+int yMax = pairs.Max(p => Math.Max(p.Beacon.Y, p.Sensor.Y));
 
-int y = 2000000;
+var ranges = GetRanges(Day1Y);
 
-List<(int StartX, int EndX)> ranges = new();
+Console.WriteLine(GetYInfo(Day1Y).count);
 
-foreach (var p in pairs)
+for (int y = yMin; y <= yMax; y++)
 {
-	int sensorDistanceY = Math.Abs(p.Sensor.Y - y);
-	if (sensorDistanceY <= p.Distance)
+	ranges = GetRanges(y);
+	(_, int? gap) = GetYInfo(y, true);
+	if (gap != null)
 	{
-		int xStart = p.Sensor.X - (p.Distance - sensorDistanceY);
-		int xEnd = p.Sensor.X + (p.Distance - sensorDistanceY);
-		ranges.Add(new (Math.Min(xStart, xEnd), Math.Max(xStart, xEnd)));
+		Console.WriteLine((long)gap * 4000000 + y);
+		break;
 	}
 }
 
-var total = 0;
-var ordered = ranges.OrderBy(x => x.StartX).ToArray();
-
-for (int i = ordered.First().StartX; i <= ordered.Last().EndX; i++)
+IEnumerable<(int XMin, int XMax)> GetRanges(int targetY)
 {
-	foreach (var range in ranges)
+	List<(int StartX, int EndX)> ranges = new();
+
+	foreach (var p in pairs)
 	{
-		if (i >= range.StartX && i <= range.EndX)
+		int sensorDistanceY = Math.Abs(p.Sensor.Y - targetY);
+		if (sensorDistanceY <= p.Distance)
 		{
-			total++;
-			break;
+			int xStart = p.Sensor.X - (p.Distance - sensorDistanceY);
+			int xEnd = p.Sensor.X + (p.Distance - sensorDistanceY);
+
+			ranges.Add(new(Math.Min(xStart, xEnd), Math.Max(xStart, xEnd)));
 		}
 	}
+
+	return ranges.OrderBy(x => x.StartX);
 }
 
-int beaconCount = pairs.Where(x => x.Beacon.Y == y).Select(x => x.Beacon).Distinct().Count();
+(int count, int? gap) GetYInfo(int y, bool isDayTwo = false)
+{
+	var rs = ranges.ToArray();
+	if (isDayTwo)
+	{
+		// Detect gap at start or end of lines.
+		int? g = rs[0].XMin > xMin ? xMin : (rs[0].XMax < xMax ? xMax : null);
+		if (g != null &&
+			g >= 0 && g <= 4000000 && y >= 0 && y <= 4000000)
+		{
+			return (-1, g);
+		}
+	}
 
-Console.WriteLine(total - beaconCount);
+	var (XMin, XMax) = rs[0];
+	for (int i = 1; i < rs.Length; i++)
+	{
+		if (rs[i].XMin > XMax + 1 && isDayTwo)
+		{
+			// Found a gap!
+			return (-1, XMax + 1);
+		}
+		XMax = Math.Max(XMax, rs[i].XMax);
+	}
+
+	int width = XMax - XMin + 1;
+	var beacons = pairs.Where(x => x.Beacon.Y == y).Select(x => x.Beacon).Distinct();
+	return isDayTwo ? (width, null) : (width - beacons.Count(), null);
+}
 
 record Coord(int X, int Y);
 struct Pair
@@ -50,3 +86,4 @@ struct Pair
 		Distance = Math.Abs(Sensor.X - Beacon.X) + Math.Abs(Sensor.Y - Beacon.Y);
 	}
 }
+
