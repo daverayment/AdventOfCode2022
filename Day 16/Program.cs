b45c3b3803742c6c93447443f41c0b78cb475662
Dave Rayment <david.rayment@experian.com>
2022-12-23 14:04:15 +0000

    Day 16 start...again.

--- Day 16/Program.cs
+++ Day 16/Program.cs
@@ -1,17 +1,17 @@
 ﻿using System.Text.RegularExpressions;
 
 var caves = File.ReadLines("Example.txt").Select(line => new Cave(line));
-
 int time = 30;
 int valvesLeft = caves.Count(x => x.FlowRate > 0);
 var current = caves.Where(x => x.Name == "AA");
-
 
 // Naive approach: multiply each linked cave's flowrate by the time remaining
 // and travel there.
 // Better: depth-first search of all linked cave's total rates (bearing in mind loopbacks)
 
 Console.WriteLine();
+
+
 public class Cave
 {
 	public string Name { get; init; }
@@ -23,5 +23,10 @@ public class Cave
 		this.Name = matches[0].Value;
 		this.LinkedCaves = matches.Skip(1).Select(x => x.Value).ToList();
 		this.FlowRate = int.Parse(Regex.Match(line, @"(\d+)").Value);
+	}
+
+	public int Score(int time)
+	{
+		return this.FlowRate * time;
 	}
 }
